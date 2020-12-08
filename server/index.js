1b8c8e592dec7ee1be1ef1ee7c47f9a27171c75e
gtretow <guilherme.tretow@gmail.com>
2020-12-07 21:37:53 -0300

    test

---
+++ server/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+const FoodModel = require("./models/Food");
+
+app.use(express.json());
+app.use(cors());
+//receber informação da front em forma de json
+mongoose.connect("mongodb://localhost:27017/food", {
+  useNewUrlParser: true,
+});
+
+app.post("/insert", async (req, res) => {
+  const foodName = req.body.foodName;
+  const days = req.body.days;
+  const food = new FoodModel({
+    foodName: foodName,
+    daysSinceIAte: days,
+  });
+  try {
+    await food.save();
+    res.send("inserted data");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get("/read", async (req, res) => {
+  FoodModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+
+    res.send(result);
+  });
+});
+
+app.put("/update", async (req, res) => {
+  const newFoodName = req.body.newFoodName;
+  const id = req.body.id;
+
+  try {
+    await FoodModel.findById(id, (err, updatedFood) => {
+      updatedFood.foodName = newFoodName;
+      updatedFood.save();
+      res.send("update");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  await FoodModel.findByIdAndRemove(id).exec();
+  res.send("deleted");
+});
+
+app.listen(1234, () => {
+  console.log("server running on port 1234");
+});
